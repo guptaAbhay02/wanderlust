2e817614ae0b49c1012da805711c42d789dd1b32
Abhay Gupta <abhaygupta@ABHAYs-MacBook-Air.local>
2025-04-14 12:20:32 +0530

    Third Commit

--- init/index.js
+++ init/index.js
@@ -2,7 +2,8 @@ const mongoose=require("mongoose");
 const initData=require("./data.js");
 const Listing=require("../models/listing.js");
 
-const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL="mongodb+srv://abhaygupta4593:sngbtwbA1tGVZgOO@cluster0.ydcrvio.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 
 main()
     .then(()=>{
