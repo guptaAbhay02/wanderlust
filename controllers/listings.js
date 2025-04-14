02a9d414673d8674c011f7491443744575d078c7
Abhay Gupta <abhaygupta@ABHAYs-MacBook-Air.local>
2025-04-14 11:49:15 +0530

    Second Commit

--- controllers/listings.js
+++ controllers/listings.js
@@ -2,7 +2,7 @@ const Listing=require("../models/listing");
 
 module.exports.index=async(req,res)=>{
     const allListings=await Listing.find({});
-    res.render("Listings/index.ejs" ,{allListings});
+    res.render("listings/index.ejs" ,{allListings});
 }
 
 module.exports.renderNewForm=(req,res)=>{
